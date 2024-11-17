8f7c8926e311a64bdc5c5c04a74abbc99ec90d10
artem1214 <61391756+artemanoh@users.noreply.github.com>
2024-11-17 12:03:24 +0200

    Update test.js

--- js/test.js
+++ js/test.js
@@ -1,28 +1,28 @@
-// const array1 = [5, 10, 15, 20];
-// const array2 = [10, 20, 30];
+const array1 = [5, 10, 15, 20];
+const array2 = [10, 20, 30];
 
-// const combinedArray = array1.concat(array2);
-// let totalSum = 0;
-// for (let i = 0; i < combinedArray.length; i++) {
-//     totalSum += combinedArray[i];
-// }
-// console.log("Сума двох масивів:", totalSum);
+const combinedArray = array1.concat(array2);
+let totalSum = 0;
+for (let i = 0; i < combinedArray.length; i++) {
+    totalSum += combinedArray[i];
+}
+console.log("Сума двох масивів:", totalSum);
 
 
-// const title = 'Top 10 benefits of React framework';
-// const slug = title
-// .toLowerCase()
-// .trim()
-// .replace(/ /g, '-'); 
-// console.log(slug);
+const title = 'Top 10 benefits of React framework';
+const slug = title
+.toLowerCase()
+.trim()
+.replace(/ /g, '-'); 
+console.log(slug);
 
-// const players = ["Artem", "Max", "Masha", "Natalia"]
-// let playersWithNumbers = []
+const players = ["Artem", "Max", "Masha", "Natalia"]
+let playersWithNumbers = []
 
-// for (let i = 0; i < players.length; i++) {
-//     playersWithNumbers.push(`${players[i]}-${i + 1}`);
-// }
-// console.log(playersWithNumbers)
+for (let i = 0; i < players.length; i++) {
+    playersWithNumbers.push(`${players[i]}-${i + 1}`);
+}
+console.log(playersWithNumbers)
 
 // додати видалити і замінити друга через splice
 // Додавання
