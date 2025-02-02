a3053f7b8d22aa3ef534300126558478fdd80105
artem1214 <61391756+artemanoh@users.noreply.github.com>
2025-02-02 10:26:36 +0200

    k

---
+++ js/dom-practice.js
@@ -0,0 +1,29 @@
+const fruits = [
+  {
+    title: "Jackfruit",
+    price: 4000,
+    photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/ba/Jackfruit_hanging.JPG/1200px-Jackfruit_hanging.JPG"
+  },
+  {
+    title: "Breadfruit",
+    price: 2500,
+    photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKMS01WCNOYUuD3Rg4CpNdWVZ-YnvYVlLopQ&s"
+  },
+  {
+    title: "Durian",
+    price: 9000,
+    photo: "https://www.foodie.com/img/gallery/what-is-durian-and-how-do-you-eat-it/intro-1703105258.jpg"
+  },
+  {
+    title:"Pneapple",
+    price: 500,
+    photo: "https://upload.wikimedia.org/wikipedia/commons/7/74/%E0%B4%95%E0%B5%88%E0%B4%A4%E0%B4%9A%E0%B5%8D%E0%B4%9A%E0%B4%95%E0%B5%8D%E0%B4%95.jpg"
+  },
+  {
+    title: "Dragonfruit",
+    price: 300,
+    photo: "https://media.post.rvohealth.io/wp-content/uploads/2024/01/A-pink-pitahaya-cut-it-in-half-Dragon-Fruit-header.jpg"
+  },
+];
+
+const fruitLayout = document.createElement('li')
